eb44f4196c6d9216e856210a0c5866b9556689c4
Dennis <antonikden@gmail.com>
2023-09-26 22:17:58 +0200

    footer update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,16 +86,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
                 href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
               },
             ],
           },
